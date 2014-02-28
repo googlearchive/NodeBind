d58d58156e18fdc8393d44b64577bb847d0b8a63
Igor Minar <igor@angularjs.org>
2014-02-28 00:13:46 -0800

    chore: remove unused Document.prototype.contains polyfill

--- src/NodeBind.js
+++ src/NodeBind.js
@@ -26,15 +26,6 @@
   }
 
 
-  // IE does not support have Document.prototype.contains.
-  if (typeof document.contains != 'function') {
-    Document.prototype.contains = function(node) {
-      if (node === this || node.parentNode === this)
-        return true;
-      return this.documentElement.contains(node);
-    }
-  }
-
   Node.prototype.bind = function(name, observable) {
     console.error('Unhandled binding to Node: ', this, name, observable);
   };
